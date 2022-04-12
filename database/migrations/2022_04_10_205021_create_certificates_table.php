70ee10378522215447bd1f2b5c5c45c081604383
Md.Rafiqul Islam <rafiqulcseduet@gmail.com>
2022-04-13 03:44:20 +0600

    after add model

---
+++ database/migrations/2022_04_10_205021_create_certificates_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('certificates', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger("NRIC_NO");
+            $table->string("School_Name");
+            $table->string("Type_Of_School");
+            $table->string("Year_Attended");
+            $table->Date("Year_Month_Form");
+            $table->Date("Year_Month_To");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('certificates');
+    }
+};
